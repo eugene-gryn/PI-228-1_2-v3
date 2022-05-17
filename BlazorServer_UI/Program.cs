0abf9066ef80d8d1b1d9049948c26ef238471f51
pinirus <zuvado11@yandex.ua>
2022-05-17 16:30:09 +0300

    Вичещення сміття з UI шару, з прикладами та нептрібними елементами.

--- BlazorServer_UI/Program.cs
+++ BlazorServer_UI/Program.cs
@@ -1,4 +1,3 @@
-using BlazorServer_UI.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
@@ -13,7 +12,7 @@ namespace Company.WebApplication1
             // Add services to the container.
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
-            builder.Services.AddSingleton<WeatherForecastService>();
+            //builder.Services.AddSingleton<WeatherForecastService>();
 
             var app = builder.Build();
 
