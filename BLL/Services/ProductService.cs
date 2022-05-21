5ebd038d36281aef20db4ce37d5838ba010dc5e6
Dmitriy Yaremenko <dmitriy@zala.com.a>
2022-05-21 10:06:01 +0300

    Change DTO

---
+++ BLL/Services/ProductService.cs
@@ -0,0 +1,20 @@
+﻿using BLL.DTOs;
+using DAL.UOW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class ProductService : AService
+    {
+        public ProductService(IUnitOfWork uow) : base(uow)
+        {
+
+        }
+
+        
+    }
+}
