31d86b33de11c93fe0ed5599b98f334b96643b45
IvanM71 <ivan.mrgun@gmail.com>
2022-05-24 19:12:01 +0300

    Some Order methods

--- BLL/Services/OrderService.cs
+++ BLL/Services/OrderService.cs
@@ -21,10 +21,28 @@ public class OrderService : AService
     }
 
     
-    public async Task<OrderDTO?> GetMainData(int OrderID)
+    public async Task<OrderDTO?> GetMainData(int orderID)
     {
-        var order = await Database.Orders.Read().FirstOrDefaultAsync(ord => ord.ID == OrderID);
+        var order = await Database.Orders.Read().AsNoTracking().FirstOrDefaultAsync(ord => ord.ID == orderID);
         if (order == null) return null;
         return Mapper.Map<OrderDTO>(order);
+    }
+
+
+    public async Task<Dictionary<ProductDTO, int>?> GetOrderProducts(int orderID)
+    {
+        var order = await Database.Orders.Read()
+            .Include(o => o.ProductAmounts)
+            .FirstOrDefaultAsync(o => o.ID == orderID);
+
+        if (order == null) return null;
+        
+        var dict = order.ProductAmounts.ToDictionary(
+            keySelector: prodAm => Mapper.Map<ProductDTO>(prodAm.Product),
+            elementSelector: prodAm => prodAm.Amount);
+
+        return dict;
+
+
     }
 }
