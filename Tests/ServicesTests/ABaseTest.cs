1ccc28bc93456319bd5ecbc728356a03d0b375e9
Nazar <dmitriy@zala.com.a>
2022-06-03 16:10:44 +0300

    Order service tests

--- Tests/ServicesTests/ABaseTest.cs
+++ Tests/ServicesTests/ABaseTest.cs
@@ -1,7 +1,10 @@
 ﻿using DAL.EF;
+using DAL.Entities;
 using DAL.UOW;
 using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
+using System.Collections.Generic;
+using System.Security.Cryptography;
 
 namespace Tests
 {
@@ -11,7 +14,17 @@ namespace Tests
         protected MainContext _context;
         protected IUnitOfWork uow;
         protected DbContextOptions<MainContext> _configuration;
-        
+
+        #region
+        private const string user1login = "aboba1@email.com";
+        private const string user2login = "aboba2@email.com";
+        private const string user3login = "aboba3@email.com";
+
+        private const string user1password = "aaa1";
+        private const string user2password = "aaa2";
+        private const string user3password = "aaa3";
+        #endregion
+
         public ABaseTest()
         {
             _configuration = new DbContextOptionsBuilder<MainContext>()
@@ -19,6 +32,20 @@ namespace Tests
             .Options;
             _context = new MainContext(_configuration);
             uow = new EFUnitOfWork(_context);
+        }
+        private static KeyValuePair<byte[], byte[]> GeneratePassword(string password)
+        {
+            byte[] passwordHash, passwordSalt;
+
+
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+
+
+            return new KeyValuePair<byte[], byte[]>(key: passwordHash, value: passwordSalt);
         }
         public virtual void EndOperation()
         {
@@ -34,7 +61,7 @@ namespace Tests
         {
             _context.Database.EnsureCreated();
 
-            TestFillDB.SeedDatabase();
+            SeedDatabase();
         }
 
         [OneTimeTearDown]
@@ -43,6 +70,130 @@ namespace Tests
             _context.Database.EnsureDeleted();
         }
 
-        
+
+        public void SeedDatabase()
+        {
+            var orders = new List<Order>()
+            {
+                new Order()
+                {
+                    ID = 1,
+                    UserID = 1,
+                    DeliveryInfo = "",
+                    Processed = true,
+                    ProductAmounts = new List<ProductAmount>(),
+                },
+                new Order()
+                {
+                    ID = 2,
+                    UserID = 1,
+                    DeliveryInfo = "",
+                    Processed = true,
+                    ProductAmounts = new List<ProductAmount>(),
+
+                },
+                new Order()
+                {
+                    ID = 3,
+                    UserID = 1,
+                    DeliveryInfo = "",
+                    Processed = true,
+                    ProductAmounts = new List<ProductAmount>(),
+                },
+            };
+            _context.Orders.AddRange(orders);
+            _context.SaveChanges();
+
+            var products = new List<Product>()
+            {
+                
+                new Product()
+                {
+                    ID = 1,
+                    Name = "",
+                    PhotoPath = "",
+                    Description = "",
+                    Price = 1f,
+                    RemainingStock = 1,
+                    Views = 0,
+                    Purchase = 0,
+                },
+                new Product()
+                {
+                    ID = 2,
+                    Name = "",
+                    PhotoPath = "",
+                    Description = "",
+                    Price = 1f,
+                    RemainingStock = 1,
+                    Views = 0,
+                    Purchase = 0,
+                },
+                new Product()
+                {
+                    ID = 3,
+                    Name = "",
+                    PhotoPath = "",
+                    Description = "",
+                    Price = 1f,
+                    RemainingStock = 1,
+                    Views = 0,
+                    Purchase = 0,
+                }
+            };
+            _context.Products.AddRange(products);
+
+            var user1 = GeneratePassword(user1password);
+            var user2 = GeneratePassword(user2password);
+            var user3 = GeneratePassword(user3password);
+
+            var users = new List<User>()
+            {
+                new User()
+                {
+                    ID = 1,
+                    Name = "test1",
+                    Email = "test1@gmail.com",
+                    Phone = "+380970000000",
+                    IsAdmin = false,
+                    IsModerator = false,
+                    PasswordHash = user1.Key,
+                    PasswordSalt = user1.Value,
+                    Cart = new List<ProductAmount>
+                    {new() {Product = products[0], Amount = 22}, new() {Product = products[1], Amount = 55}},
+
+                },
+                new User()
+                {
+                    ID = 2,
+                    Name = "test2",
+                    Email = "test2@gmail.com",
+                    Phone = "+380970000000",
+                    IsAdmin = false,
+                    IsModerator = false,
+                    PasswordHash = user2.Key,
+                    PasswordSalt = user2.Value,
+                    Cart = new List<ProductAmount>
+                    {new() {Product = products[0], Amount = 22}, new() {Product = products[1], Amount = 55}},
+                },
+                new User()
+                {
+                    ID = 3,
+                    Name = "test3",
+                    Email = "test3@gmail.com",
+                    Phone = "+380970000000",
+                    IsAdmin = false,
+                    IsModerator = false,
+                    PasswordHash = user2.Key,
+                    PasswordSalt = user2.Value,
+                    Cart = new List<ProductAmount>
+                    {new() {Product = products[0], Amount = 22}, new() {Product = products[1], Amount = 55}},
+                }
+            };
+            _context.Users.AddRange(users);
+
+            _context.SaveChanges();
+        }
+
     }
 }
--- Tests/ServicesTests/OrderServicesTest.cs
+++ Tests/ServicesTests/OrderServicesTest.cs
@@ -1,18 +1,159 @@
-﻿using BLL.Services;
-using DAL.EF;
-using DAL.UOW;
-using Microsoft.EntityFrameworkCore;
+﻿using BLL.DTOs;
+using BLL.DTOs.User;
+using BLL.Services;
+using NUnit.Framework;
 
 namespace Tests.Services
 {
     public class OrderServicesTest : ABaseTest
     {
+
         private OrderService _repos;
+        public OrderServicesTest()
+        {
+            _repos = new OrderService(uow);
+        }
 
         public override void EndOperation()
         {
+
             base.EndOperation();
             _repos = new OrderService(uow);
+        }
+        [Test]
+        public void CreateOrder_InNotEmptyAsync()
+        {
+            OrderDTO order = new OrderDTO();
+
+             _repos.Create(order);
+
+            Assert.AreEqual(order.DeliveryInfo,"");
+            EndOperation();
+        }
+        [Test]
+        public void GetMainData_InNotEmptyAsync()
+        {
+            OrderDTO order = new OrderDTO();
+
+         
+            _repos.Create(order);
+            _repos.GetMainData(order.ID);
+
+            Assert.IsNotNull(order);
+            EndOperation();
+
+        }
+        [Test]
+        public void GetOrderProduct_InNotEmpty()
+        {
+            OrderDTO order = new OrderDTO();
+
+            _repos.Create(order);
+            _repos.GetOrderProducts(order.ID);
+
+            Assert.IsNotNull(order);
+            EndOperation();
+
+        }
+        [Test]
+        public void GetUserOrders_InNotEmpty()
+        {
+            OrderDTO order = new OrderDTO();
+            
+
+            _repos.Create(order);
+            _repos.GetUserOrders(order.ID);
+
+            Assert.IsNotNull(order);
+            EndOperation();
+        }
+        [Test]
+        public void GetCartProducts_InNotEmpty()
+        {
+            OrderDTO order = new OrderDTO();
+            UserMainDataDTO user = new UserMainDataDTO();
+
+            _repos.Create(order);
+            _repos.GetCartProducts(user.ID);
+
+            Assert.IsNotNull(user);
+            EndOperation();
+        }
+        [Test]
+        public void DeleteOrder_InNotEmpty()
+        {
+            OrderDTO order = new OrderDTO();
+
+            _repos.Create(order);
+            _repos.DeleteOrder(order.ID);
+
+            Assert.IsNotNull(order);
+            EndOperation();
+        }
+        [Test]
+        public void MoveProductsFromCartToOrder_InNotEmpty()
+        {
+            OrderDTO order = new OrderDTO();
+            UserMainDataDTO user = new UserMainDataDTO();
+            ProductDTO product = new ProductDTO();
+
+            _repos.Create(order);
+            _repos.AddProductToCart(user.ID,product.ID,1);
+
+            Assert.IsNotNull(order);
+            EndOperation();
+        }
+        [Test]
+        public void AssignProductsToOrder_InNotEmpty()
+        {
+            OrderDTO order = new OrderDTO();
+            UserCartDTO cart = new UserCartDTO();
+            ProductDTO product = new ProductDTO();
+
+            _repos.Create(order);
+            _repos.AssignProductsToOrder(order.ID, cart.Cart);
+
+            Assert.IsNotNull(cart);
+            EndOperation();
+        }
+        /**/
+        [Test]
+        public void MarkOrderAsProcessed_IsProcesed()
+        {
+            OrderDTO order = new OrderDTO();
+
+            _repos.Create(order);
+            
+            _repos.MarkOrderAsProcessed(order.ID);
+
+            Assert.IsFalse(order.Processed);
+            EndOperation();
+        }
+        [Test]
+        public void AddProductToCart_InNotEmpty()
+        {
+            OrderDTO order = new OrderDTO();
+            UserMainDataDTO user = new UserMainDataDTO();
+            ProductDTO product = new ProductDTO();
+
+            _repos.Create(order);
+            _repos.AddProductToCart(user.ID, product.ID, 1);
+
+            Assert.IsNotNull(order);
+            EndOperation();
+        }
+        [Test]
+        public void DeleteProductFromCart_InNotEmpty()
+        {
+            OrderDTO order = new OrderDTO();
+            UserMainDataDTO user = new UserMainDataDTO();
+            ProductDTO product = new ProductDTO();
+
+            _repos.Create(order);
+            _repos.DeleteProductFromCart(user.ID,product.ID);
+
+            Assert.IsNotNull(order);
+            EndOperation();
         }
     }
 }
